9d44b0115d5080a35a01fb03d88c20a46e04d021
Mack Benham <macpaper23@yahoo.com>
2024-10-20 22:24:22 -0400

    cleaned stuff up and rewrote stuff

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import random
 import math
-# random.seed(random.randint(0,10000000))
 
-# for i in range(1000):
-#     print(random.uniform(0, 1))
+# Finding the inverse CDF of exponential random variable X with lambda = 1/12, E(X) = 12
 # F(x) = u
 # x = F^-1(u)
 # u = 1 - e^(-1/12x)
@@ -24,66 +21,78 @@ def generateNthRandNum(n):
         u = x / K
     return u
 
-# generate a realization of x_i
-# x_i = minimum x for which F(x) >= u_i
-# you can find this value by sequentially searching 
-# over x = 1...k until F(x) >= u_i for the first time
+def customerIsAvailable(i): 
+    x = -math.log(1-generateNthRandNum(i)) * 12
+    return x
 
+# Generate a list of random variables from (0, 1) using random number generator.
+u_list = []
+for i in range(1, 10002):
+    u_list.append(generateNthRandNum(i))
 
-print(generateNthRandNum(1))
+# u1, u2, u3 and u51, u52, u53 are printed here
+print(generateNthRandNum(1))    
 print(generateNthRandNum(2))
 print(generateNthRandNum(3))
-u51 = generateNthRandNum(51)
-u52 = generateNthRandNum(52)
-u53 = generateNthRandNum(53)
-print(u51)
-print(u52)
-print(u53)
+print(generateNthRandNum(51))
+print(generateNthRandNum(52))
+print(generateNthRandNum(53))
+
+# Fields for phone time
 TIME_TO_PICK_UP_PHONE = 6
 TIME_TO_GET_BUSY_SIGNAL = 3
 TIME_TO_END_CALL = 1
 TIME_TO_HEAR_5_RINGS = 25
-mean = 12
-lam = 1/mean
-def sumX():
-    sum = 0
-    for i in range(1, 6):
-        sum += generateNthRandNum(i)
-    return sum
-         
-min(sumX(), generateNthRandNum(5))
-def callCustomer():
-    # W = random variable. # of seconds spent calling customer
+
+mean = 12 # Expected time it takes for available customer to pick up phone is 12 seconds
+lam = 1/mean 
+
+# Generates a call for a single customer
+def callCustomer(i):
+    # w is number of seconds spent calling customer i
     w = TIME_TO_PICK_UP_PHONE
-    lol = random.randint(0, 10000)
-    for i in range(lol, lol+4):
-        callProbability = generateNthRandNum(i)
+    timesCalled = 0
+    iter = 0
+    while timesCalled < 4:
+        timesCalled += 1
+        callProbability = generateNthRandNum(i + iter)
         if callProbability <= 0.2:
             w += TIME_TO_GET_BUSY_SIGNAL
-            break
         elif callProbability <= 0.5:
             w += TIME_TO_HEAR_5_RINGS
         else:
             # customer is AVAILABLE
-            timeToAnswer = customerIsAvailable(i)
-            if timeToAnswer >= 25:
+            timeToAnswer = customerIsAvailable(i + iter)
+            if timeToAnswer > 25:
                 w += TIME_TO_HEAR_5_RINGS
             else:
                 w += timeToAnswer
                 break
+        iter += 1
     w += TIME_TO_END_CALL
+    return w
 
-def exponentialCDFatX(lam, x):
-    cdfAtX = 1 - math.e**(-lam * x)
-    return cdfAtX
+w_list = []
+for w in range(1, 501):
+    val = callCustomer(w)
+    w_list.append(val)
+    if val > 107:
+        print("OH NO SOMETHING WENT HORRIBLY WRONG")
+print(w_list)
 
-def customerIsAvailable(i): 
-    x = -math.log(1-generateNthRandNum(i)) * 12
-    math.log(127891)
-    math.log(15)
-    math.log(4)
-    math.log(98)
-    print(x)
-    return x
-# for i in range(10):
-    # callCustomer()
+
+# SOME NOTES:
+# There are a lot of repeating values of W. Consider this:
+# There is a probability that the customer is ALWAYS busy. This is 6 + 3*4 + 1 = 19
+# There is a probability that the customer is ALWAYS unavailable. This is 6 + 25*4 + 1 = 107
+# There is a probability that the customer is ALWAYS some combination of busy or unavailable. 
+#   This is a set of whole numbers between 19 and 107 with a combination of 4(c) + 25(1-c) 
+#   where c is the number of times called.
+# Any other number is the time it takes the customer to answer before 25 seconds.
+#   This number should always be less than 107. 
+# Every single w should be less than or equal to 107 (verified above this btw)
+
+# probably dont need this lol
+# def exponentialCDFatX(lam, x):
+#     cdfAtX = 1 - math.e**(-lam * x)
+#     return cdfAtX
---
+++ second.py
@@ -0,0 +1,16 @@
+
+# def generateDiscreteVar(i):
+#     # x = 0
+#     ui = generateNthRandNum(i)
+    
+#     xi = ...
+
+#     for k in range(1, i+1):
+#         x += generateNthRandNum(i)
+#         if x >= ui:
+#             return 
+
+# generate a realization of x_i
+# x_i = minimum x for which F(x) >= u_i
+# you can find this value by sequentially searching 
+# over x = 1...k until F(x) >= u_i for the first time
